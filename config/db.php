ebf2f54ff3deb4f3ff6ca1f411faeecdd55722b5
Sem765887 <semyon.gardt765887@mail.ru>
2025-06-17 13:50:47 +0700

    server push

--- config/db.php
+++ config/db.php
@@ -1,10 +1,10 @@
 <?php
 $host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "laboratory";
+$user = "fjttkvkf";
+$password = "Ven5U5";
+$db = "fjttkvkf_m4";
 
-$conn = new mysqli($host, $user, $password, $dbname);
+$conn = new mysqli($host, $user, $password, $db);
 
 if ($conn->connect_error) {
     die("Ошибка подключения: " . $conn->connect_error);
